c05575e38ce8b2ed61534f675ee73e448406a96e
lklindar <lindarkusuma@student.uns.ac.id>
2022-04-22 19:04:18 +0700

    first react

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import { Container, Row, Col } from "reactstrap";
+
+import Header from "./components/Header.js";
+import SideCard from "./components/Sidecard.js";
+import Post from "./components/Post.js";
+
+const App = () => (
+  <>
+    <Header />
+
+    <main className="my-5 py-5">
+      <Container className="px-0">
+        <Row
+          noGutters
+          className="pt-2 pt-md-5 w-100 px-4 px-xl-0 position-relative"
+        >
+          <Col
+            xs={{ order: 2 }}
+            md={{ size: 4, order: 1 }}
+            tag="aside"
+            className="pb-5 mb-5 pb-md-0 mb-md-0 mx-auto mx-md-0"
+          >
+            <SideCard />
+          </Col>
+
+          <Col
+            xs={{ order: 1 }}
+            md={{ size: 7, offset: 1 }}
+            tag="section"
+            className="py-5 mb-5 py-md-0 mb-md-0"
+          >
+            <Post />
+          </Col>
+        </Row>
+      </Container>
+    </main>
+  </>
+);
+
+export default App;
---
+++ src/components/Sidecard.js
@@ -0,0 +1,49 @@
+import React, { Fragment } from "react";
+
+import {
+  Button,
+  UncontrolledAlert,
+  Card,
+  CardImg,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  CardText
+} from "reactstrap";
+
+const BANNER = "https://i.imgur.com/CaKdFMq.jpg";
+
+const SideCard = () => (
+  <Fragment>
+    <UncontrolledAlert color="danger" className="d-none d-lg-block">
+      <strong>Account not activated.</strong>
+    </UncontrolledAlert>
+
+    <Card>
+      <CardImg top width="100%" src={BANNER} alt="banner" />
+      <CardBody>
+        <CardTitle className="h3 mb-2 pt-2 font-weight-bold text-secondary">
+          Stephany Johnson
+        </CardTitle>
+        <CardSubtitle
+          className="text-secondary mb-3 font-weight-light text-uppercase"
+          style={{ fontSize: "0.8rem" }}
+        >
+          Web Developer, Los Angeles
+        </CardSubtitle>
+        <CardText
+          className="text-secondary mb-4"
+          style={{ fontSize: "0.75rem" }}
+        >
+          Full-stack web developer learning new hacks one day at a time. Web
+          technology enthusiast. Hacking stuffs @stephany_johnson.
+        </CardText>
+        <Button color="success" className="font-weight-bold">
+          View Profile
+        </Button>
+      </CardBody>
+    </Card>
+  </Fragment>
+);
+
+export default SideCard;
